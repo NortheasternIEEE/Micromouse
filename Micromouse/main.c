1ddf6905a82549fad69dfcff457339439aa1bb13
Matthew <murphy1022@gmail.com>
2016-02-15 18:46:44 -0500

    Initial Commit

---
+++ Micromouse/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Micromouse
+//
+//  Created by Matthew Murphy on 2/15/16.
+//  Copyright © 2016 Northeastern-IEEE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
