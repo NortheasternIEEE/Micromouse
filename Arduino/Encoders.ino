dad8015be7f346bef6fd9e5e247549379f38a573
NUMicroMouse <camaj.a@husky.neu.edu>
2016-03-26 12:11:32 -0400

    Update Encoders.ino

--- Arduino/Encoders.ino
+++ Arduino/Encoders.ino
@@ -13,24 +13,51 @@
 //side without button I am calling the right
 //Left encoder is denoted as 0
 //Right encoder is Denoted as 1
-//Encoder 0 has OUT A going to pin 19
-//Encoder 0 has OUT B going to pin 21
-
-//Encoder 1 has OUT A going to pin 20
-//Encoder 1 has OUT B going to pin 12
 
 #include "Arduino.h";
-#include <digitalWriteFast.h>
 
-int leftEncoder_PIN_A = 19;
-int leftEncoder_PIN_B = 21;
-int rightEncoder_PIN_A = 20;
-int rightEncoder_PIN_B = 12;
+#define leftEncoder_PIN_A = 12;
+#define rightEncoder_PIN_A = 23;
+
+//distance betwen cells is 18 cm
+
+#define distanceBetweenCells = .18;
+
+//diameter of the wheel
+#define diameterOfWheel = .45
+
+//circumference of the wheel is pie * diameter
+#define circumferenceOfWheel = diameterOfWheel*3.14159
+
+/*The reason we chose 50 is the 50 to 1 gear motor, and teh 3 comes \
+ * from the encoder
+ */
+#define encoderTicksOneRevolution = 50*3;
+//encoderTicks = (360/circumferenceOfWheel)*distance to travel;
+//encoder ticks are the same thing as counts per revolution
+
+//50*3*18/12.5666
+#define ticksInOneCell = 215;
+int wheelRotations = 0;
+
+/*We know that the distance traveled in one rotation
+is equal to pi*diameter of the wheel, and if they rotate twice we 
+get twice that amount.
+*/
+
+//forward is equal to 1; reverse is equal to 0
+#define forward = 1;
+#define reverse = 0;
+
+int directionOfBot = forward;
+
+int distanceMovedLeft = 0;
+int distanceMovedRight = 0;
+
+#define avgDist() ((distanceMovedLeft + DistanceMovedRight) / 2)
 
 volatile int leftEncoderOutA_State = LOW;
-volatile int leftEncoderOutB_State = LOW;
 volatile int rightEncoderOutA_State = LOW;
-volatile int rightEncoderOutB_State = LOW;
 
 void setup() {
   //since working with the zero micro we can do the folowing
@@ -38,37 +65,46 @@ void setup() {
   //ISR is simply the function that returns no parameters and is called when mode occurs
   //mode defines when interrupt is triggerd
   //see page 364 of http://www.atmel.com/Images/Atmel-42181-SAM-D21_Datasheet.pdf
+
+  //just declare the number of wheel rotations
+  
   
   attachInterrupt(leftEncoder_PIN_A, isr_leftEncoder_OUTA, CHANGE);
-  attachInterrupt(leftEncoder_PIN_B, isr_leftEncoder_OUTB, CHANGE);
   attachInterrupt(rightEncoder_PIN_A, isr_rightEncoder_OUTA, CHANGE);
-  attachInterrupt(rightEncoder_PIN_B, isr_rightEncoder_OUTB, CHANGE);
+  //pin 15 for the capture timer interupt
+  EVCTRL.TCEI = 1;
+
+  //Pin 28 on the MCU coresspond to I/O TCC0/WO[3]
+  //
+
+  
+  //EVCTRL.MCEIx = 1;
+  //CTRLC.CAPTENx = 1;
+  //we want TCCx EV1 which will take the period and pulse width
+  //pin 15
+  //
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (leftEncoderOutA_State == HIGH)
-  {
-    //add distance of movement
+  while (avgDist() < ticksInOneCell) {
+    
   }
+  distanceMovedLeft = 0;
+  distanceMovedRight = 0;
+  
 }
 
 void isr_leftEncoder_OUTA ()
 {
-  leftEncoderOutA_State = HIGH;
-}
-
-void isr_leftEncoder_OUTB ()
-{
-  
+  distanceMovedLeft += 1;
 }
 
 void isr_rightEncoder_OUTA ()
 {
-  
+  distanceMovedRight += 1;
 }
 
-void isr_rightEncoder_OUTB ()
+void driveOneCell()
 {
   
 }
